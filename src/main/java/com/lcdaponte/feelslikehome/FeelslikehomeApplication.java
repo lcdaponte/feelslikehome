aaaf1dd4860acfe6b573872448537ab7e0552345
Lucas Cavalcante <lcdaponte@gmail.com>
2021-06-13 13:54:16 -0300

    Initial commit

---
+++ src/main/java/com/lcdaponte/feelslikehome/FeelslikehomeApplication.java
@@ -0,0 +1,13 @@
+package com.lcdaponte.feelslikehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelslikehomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeelslikehomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcdaponte/feelslikehome/FeelslikehomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcdaponte.feelslikehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelslikehomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
